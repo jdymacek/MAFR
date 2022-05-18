33edb5d01ac9a4132e1a5abeea2077e9fee4b007
EddyJohn51 <john.k.eddy.51@gmail.com>
2022-05-18 15:09:08 -0400

    updated test for MAFR

--- testMAFR.py
+++ testMAFR.py
@@ -1,21 +1,33 @@
 import MAFR
 import random
 from PIL import Image
+import numpy as np
 
 #img is 542, 260
-img = MAFR.loadImage('test.png', 16)
+img = MAFR.loadImage('test-image.png', 16)
 img.save('cropped.png')
 print(f'WIDTH: {img.width}\nHEIGHT: {img.height}')
 
 matrix = MAFR.imageToMatrix(img, 16)
 print(matrix)
 
-MAFR.saveMatrix(matrix, 64, 16, 2, 'test-dir')
+#a = np.random.rand(5,5)
 
-test = MAFR.loadMatrix('test-dir+16+16+64.nmf')
+a = [[0,0,0,0],[1,1,1,1],[2,2,2,2],[3,3,3,3]]
+nd_a = np.array(a, dtype=np.double)
 
-if test == matrix:
-  print('Success!')
+MAFR.saveMatrix(nd_a, 4, 2, 'OVEN', 'test-dir')
+test = MAFR.loadMatrix('test-dir+2+2+4.nmf')
+
+print(nd_a)
+print(test)
+
+if np.array_equal(nd_a, test):
+  print('same')
 else:
-  print('Aw man :(')
+  print('oops')
 
+b = [[0,0,0,0],[1,1,1,1],[2,2,2,2],[3,3,3,3]]
+nd_b = np.array(b, dtype=np.double)
+
+print(MAFR.computeError(a,b))
