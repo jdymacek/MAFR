4b112d4230509d751c280c4ed7c50a0fafad168b
EddyJohn51 <john.k.eddy.51@gmail.com>
2022-05-19 15:44:08 -0400

    updated patternmaker

--- src/patternMaker.py
+++ src/patternMaker.py
@@ -5,6 +5,7 @@ import numpy as np
 from MAFR import loadImage, imageToMatrix, saveMatrix, matrixToImage
 from sklearn import decomposition
 from PIL import Image
+import random
 
 cwd = os.getcwd()
 parser = argparse.ArgumentParser()
@@ -22,19 +23,23 @@ parser.add_argument('-f', required=False, default = 'foo', help = '')
 
 parser.add_argument('-s', help='species code for incoming data')
 
+parser.add_argument('-n', help='number of images to use in pattern creation', default=-1)
+
 arg = parser.parse_args()
 print(arg)
 
 
 n_components = int(arg.p)
 blockSize = int(arg.b)
+img_num = int(arg.n)
 
 #M = np.ndarray((1, blockSize*blockSize))
 ml = []
 file_list = os.listdir(arg.d)
-print(f'Initial bigM:\n{ml}')
 
-for filename in file_list:
+matrices = random.sample(file_list, img_num)
+
+for filename in matrices:
      if filename.endswith(".png"):
         #looking for the patterns in each png file
         fname_path = arg.d + '/' + filename
@@ -43,17 +48,14 @@ for filename in file_list:
         #pattern filw should involve the objec file name given by -o
         #put the files in an output directory
         ml += [m]  
-
 M = np.concatenate(ml)
-print(M.shape)
+
 estim = decomposition.NMF(n_components=n_components, init='random', random_state=0, max_iter=10000)
 w = estim.fit_transform(M)
 print(f'Iterations: {estim.n_iter_}')
 print(f'ERROR: {estim.reconstruction_err_}')
 h = estim.components_
-saveMatrix(h, arg.p, arg.b, arg.s, arg.d)
-pattern_img = matrixToImage(h,1,len(h))
-pattern_img.save('patterns.png')
+saveMatrix(h, arg.p, arg.b, arg.s, out=arg.o)
 
 
 
