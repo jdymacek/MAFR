baa02c55b841093f866a652987668eb0699f6725
EddyJohn51 <john.k.eddy.51@gmail.com>
2022-05-20 12:43:55 -0400

    naiveDir

--- src/naiveDir.py
+++ src/naiveDir.py
@@ -14,7 +14,7 @@ parser=argparse.ArgumentParser()
 #directory of files to be tested, directory to .nmf files, species code,
 #number of patterns
 
-parser.add_argument('-i', metavar='directory of .png files', default=cwd, help='path to directory containing .png files for testing')
+parser.add_argument('-i', metavar='testing directory', default=cwd, help='path to directory containing .png files for testing')
 parser.add_argument('-d', metavar='directory of .nmf files', default=cwd, help='path to directory containging .nmf files')
 parser.add_argument('-b', metavar='block size', default=16, help='blocksize of patterns')
 parser.add_argument('-o', metavar='output directory', default=cwd, help='path to where output files should go')
@@ -22,18 +22,21 @@ parser.add_argument('-o', metavar='output directory', default=cwd, help='path to
 args = parser.parse_args()
 
 patternDir = args.d
-imageDir = args.i
+testingDir = args.i
 blockSize = int(args.b)
 outputDir = args.o
-species = imageDir[-5:-1]
-print(species)
 
-pngs = os.listdir(imageDir)
+dirs = os.listdir(testingDir)
+for d in dirs:
+  dPath = testingDir + '/' + d
+  pngs = os.listdir(dPath)
+  species = dPath[-4:]
+  print(species)
 
-for img in pngs:
-  imgPath = imageDir + '/' + img
-  outputPath = outputDir + '/' + 'naive-' + species + '-' + img + '-' + str(blockSize) + '.txt'
+  outputPath = outputDir + '/' + 'naive-' + species + '-' + str(blockSize) + '.txt'
   with open(outputPath, 'w') as f:
     f.write(f'{species}\t')
-  os.system(f'python3 naiveclassifier.py -d {patternDir} -f {imgPath} -b {str(blockSize)} >> {outputPath}')
+    for img in pngs:
+      imgPath = dPath + '/' + img
+      os.system(f'python3 naiveclassifier.py -d {patternDir} -f {imgPath} -b {str(blockSize)} >> {outputPath}')
 
