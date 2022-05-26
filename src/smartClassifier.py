e6e0c6abcaf5b28c9070851947c76f6a903ae79d
EddyJohn51 <john.k.eddy.51@gmail.com>
2022-05-26 13:39:53 -0400

    smart

--- src/smartClassifier.py
+++ src/smartClassifier.py
@@ -21,6 +21,44 @@ TODO:
     - want to see how long it takes to run after loading all files 
 '''
 
+def trainingError(curated, trainingPath):
+    t0 = time.time()
+    training = os.listdir(trainingPath)
+
+    errors = []
+    correct = 0
+    total = 0
+
+    for d in training:
+        path = trainingPath + '/' + d
+        images = os.listdir(path)
+        for i in images:
+            if i.endswith('.png'):
+                img = MAFR.loadImage(path + '/' + i, 16)
+                original = MAFR.imageToMatrix(img, 16)
+                for key in curated:
+                    m = curated[key][0]
+                    pInv = curated[key][1]
+                    s = curated[key][2]
+                    error = MAFR.quickError(original, m, pInv)
+                    errors.append((s, error))
+
+	
+  
+        errors.sort(key=operator.itemgetter(1))
+
+        if errors[0][0] == d:
+            correct += 1
+        total += 1
+
+        
+    print(f'{correct} right out of {total}')
+    print(f'{correct/total} percent')
+    print(f'TIME: {time.time() - t0}')
+    return correct/total
+
+
+
 parser = argparse.ArgumentParser(description='Smart(ish) Classifier')
 parser.add_argument('-d', help='directory of desired .nmf pattern files') #patterns
 parser.add_argument('-f', help='path to training set') #file to be tested
@@ -44,43 +82,9 @@ for p in patterns:
         else:
             speciesDict[s].append(t)
 
-t0 = time.time()
 curated = {}
 
 for key in speciesDict:
     curated[key] = speciesDict[key][np.random.randint(len(speciesDict[key]))]
 
-training = os.listdir(trainingDir)
-
-errors = []
-correct = 0
-total = 0
-
-for d in training:
-    path = trainingDir + '/' + d
-    images = os.listdir(path)
-    for i in images:
-        if i.endswith('.png'):
-            img = MAFR.loadImage(path + '/' + i, 16)
-            original = MAFR.imageToMatrix(img, 16)
-            for key in curated:
-                m = curated[key][0]
-                pInv = curated[key][1]
-                s = curated[key][2]
-                error = MAFR.quickError(original, m, pInv)
-                errors.append((s, error))
-
-	
-  
-        errors.sort(key=operator.itemgetter(1))
-
-        if errors[0][0] == d:
-            correct += 1
-        total += 1
-
-print(f'{correct} right out of {total}')
-print(f'{correct/total} percent')
-print(f'TIME: {time.time() - t0}')
-
-
-    
+trainingError(curated, trainingDir)
