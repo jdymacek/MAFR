2dd1aa44a582e52c8af6a6b41c27b329843ba814
EddyJohn51 <john.k.eddy.51@gmail.com>
2022-06-09 14:29:10 -0400

    efficiency

--- src/hitPattern.py
+++ src/hitPattern.py
@@ -7,6 +7,8 @@ import random
 import math
 from sklearn import decomposition
 
+
+
 BLOCKSIZE = 16
 PATTERNS = 16
 PATH = "/scratch/prism2022data/annotatedInverseDir/"
@@ -25,6 +27,10 @@ matrices = {k:"" for k in classes}
 for c in classes:
     matrices[c] = MAFR.imageToMatrix(MAFR.loadImage(paths[c], BLOCKSIZE), BLOCKSIZE)
 
+
+currEstim = decomposition.NMF(n_components=PATTERNS*len(classes), init="random", random_state=0, max_iter=10000, solver="mu")
+currEstim.fit(matrices["AMRE"])
+
 annotation = []
 ml = []
 currentPatterns = {k:"" for k in classes}
@@ -38,34 +44,8 @@ for k,v in matrices.items():
     for j in range(PATTERNS):
         annotation.append(k)
 
-currentState = np.concatenate(ml)
-
-initialTemp = 5
-finalTemp = 0.1
-alpha = 0.01
-currentTemp = initialTemp
-
-'''
-bestScore = 0
-bestPatterns = ""
-'''
-while(currentTemp > finalTemp):
-
-    neighborPatterns = currentPatterns
-    toChange = random.choice(classes)
-    np.random.shuffle(matrices[toChange])
-    newBlocks = matrices[toChange][:32]
-    model.fit_transform(newBlocks)
-    neighborPatterns[toChange] = model.components_
-
-    ml = []
-    for k,v in neighborPatterns.items():
-        ml += [v]
-    neighbor = np.concatenate(ml)
-
-    currEstim = decomposition.NMF(n_components=PATTERNS*len(classes), init="random", random_state=0, max_iter=10000, solver="mu")
-    currEstim.fit(matrices["AMRE"])
-    currEstim.components_ = currentState
+def calcScore(state):
+    currEstim.components_ = state
 
     total = 0
     for k,v in matrices.items():
@@ -82,36 +62,51 @@ while(currentTemp > finalTemp):
             hits[ss[0][1]] += 1 
         total += hits[correct]
     currentScore = 1 - (total/(len(classes)*256))
-    
-    neighborEstim = decomposition.NMF(n_components=PATTERNS*len(classes), init="random", random_state=0, max_iter=10000, solver="mu")
-    neighborEstim.fit(matrices["AMRE"])
-    neighborEstim.components_ = neighbor
+    return currentScore * 1000
 
-    neighborTotal = 0
-    for k,v in matrices.items():
-        W = neighborEstim.transform(v)
-        correct = k
-        hits = {c:0 for c in classes}
-        for block in W:
-            percents = {c:0 for c in classes}
-            block /= np.sum(block)
-            for j in range(0, len(annotation)):
-                percents[annotation[j]] += block[j]
-            ss = [(x,y) for y,x in percents.items()]
-            ss.sort(reverse=True)
-            hits[ss[0][1]] += 1 
-        neighborTotal += hits[correct]
-    neighborScore = 1 - (neighborTotal/(len(classes)*256))
+
+currentState = np.concatenate(ml)
+currentScore = calcScore(currentState)
+solution = currentState
+solutionScore = currentScore
+
+initialTemp = 20
+finalTemp = 0.1
+alpha = 0.1
+currentTemp = initialTemp
+
+while(currentTemp > finalTemp):
+
+    neighborPatterns = currentPatterns
+    toChange = random.choice(classes)
+    np.random.shuffle(matrices[toChange])
+    newBlocks = matrices[toChange][:32]
+    model.fit_transform(newBlocks)
+    neighborPatterns[toChange] = model.components_
+
+    ml = []
+    for k,v in neighborPatterns.items():
+        ml += [v]
+    neighbor = np.concatenate(ml)
+
+    
+    neighborScore = calcScore(neighbor)
 
     diff = neighborScore - currentScore
-    diff *= 100
+
     print(f"CURR: {currentScore}\tNEIGHBOR: {neighborScore}\tTEMP: {currentTemp}")
 
     if diff < 0:
         currentState = neighbor
+        currentScore = neighborScore
     else:
         if random.uniform(0,1) < math.exp(-diff/currentTemp):
             currentState = neighbor
+            currentScore = neighborScore
+
+    if currentScore < solutionScore:
+        solution = currentState
+        solutionScore = currentScore
     currentTemp -= alpha
 
 MAFR.saveHitPatterns(solution, annotation, BLOCKSIZE)
