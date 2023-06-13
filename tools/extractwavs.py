5053d7e765a723983311e660ae9223ee1b4cf04a
Joshua Mealy <joshme@sutherland.cs.longwood.edu>
2023-06-13 13:57:13 -0400

    added flag

--- tools/extractwavs.py
+++ tools/extractwavs.py
@@ -7,6 +7,7 @@ from scipy.io.wavfile import write
 parser = argparse.ArgumentParser(description= 'Get all h5 files')
 parser.add_argument('-d', help='directory for h5 files')
 parser.add_argument('-o', help='output directory')
+parser.add_argument('-r', help='reject file')
 args = parser.parse_args()
 
 h5Dir = args.d
@@ -15,6 +16,13 @@ outputDir = args.o
 species = {"1-1-2": "CHSP", "1-1-3":"SAVS", "1-4-1" :"AMRE", "1-4-2":"BBWA", "1-4-3":"BTBW", "1-4-5":"COYE", "1-4-7":"OVEN"}
 
 allFiles = [x[0] + "/" + y for x in os.walk(h5Dir) for y in x[2] if y.endswith("_original.h5") and y[-17:-12] in species ]
+
+cullSet = set()
+with open(args.r, 'r') as rejectFile:
+    lines = rejectFile.readlines()
+    cullSet = set([x.strip() for x in lines ]) 
+    
+
 
 
 for file in allFiles:
@@ -27,21 +35,24 @@ for file in allFiles:
     except FileExistsError:
         pass
     with h5py.File(file, "r") as h5:
-        print(h5.keys())
-        print(h5["sample_rate"][()])
-        print(h5["variant"][()])
-        print(h5["taxonomy_code"][()])
-        print(h5["dataset"][()])
+
+        #print(h5.keys())
+        #print(h5["sample_rate"][()])
+        #print(h5["variant"][()])
+        #print(h5["taxonomy_code"][()])
+        #print(h5["dataset"][()])
         for k in h5['waveforms'].keys():
-            dataset = file[:-18].split("/")[-1]
+            if k not in cullSet:
+                 dataset = file[:-18].split("/")[-1]
             
-            sp = species[file[-17:-12]]
-            data = h5['waveforms'][k][:]
-            print(data.dtype)
-            write(outputDir + sp + "/" + k + ".wav", 22050, data)
+                 sp = species[file[-17:-12]]
+                 data = h5['waveforms'][k][:]
+                 
+           # print(data.dtype)
+                 write(outputDir + sp + "/" + k + ".wav", 22050, data)
             #with open("temp/" + k + ".wav","wb") as outfile:
-                #outfile.write(data.tobytes())
-                
+            else:
+                 print(k)
 
 
 
