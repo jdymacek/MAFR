0c69d36dd0d5b9ad9495a214595463f698948290
julian dymacek <dymacekjm@longwood.edu>
2023-06-14 13:17:49 -0400

    added Timer.py

---
+++ src/eigenNFC/Timer.py
@@ -0,0 +1,23 @@
+import time
+
+class Timer:
+	def __init__(self):
+		self._startTime = None
+		self._elapsed = None
+
+
+	def start(self):
+		self._startTime = time.perf_counter()
+		self._elapsed = None
+
+	def stop(self):
+		if self._startTime == None:
+			return None
+		self._elapsed = time.perf_counter() - self._startTime
+		self._startTime = None
+		return self._elapsed
+
+	def print(self):
+		if self._elapsed == None
+			return f"Total Elapsed Time: None"
+		return f"Total Elapsed Time: {self._elapsed:0.4f}s"
