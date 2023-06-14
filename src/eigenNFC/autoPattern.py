5de639fafcebd01ab088cdf33f8b86fd5f0c008f
Joshua Mealy <joshme@sutherland.cs.longwood.edu>
2023-06-14 13:33:03 -0400

    added timer stuff``

--- src/eigenNFC/autoPattern.py
+++ src/eigenNFC/autoPattern.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import argparse
 import MAFR
+from Timer import Timer
+
 
 parser = argparse.ArgumentParser("Testing harness for MAFR")
 parser.add_argument("-n", help="range of pattern values", required=True)
@@ -19,15 +21,22 @@ r = 116
 
 allFiles, classes = MAFR.listAllFiles(args.d)
 allTraining, allTesting = MAFR.splitSamples(allFiles, classes, float(args.t))
+
+stopwatch = Timer()
 for p in PATTERNS:
+    stopwatch.start()
     trainer = SimpleTrainer(allTraining, p) 
     trainer.updateSize((256-r), w)
+    stopwatch.stop()
+    stopwatch.print()
+    stopwatch.start()
     classifier = EigenAverage(classes, p, w, (256 - r))
     weights, patterns = trainer.train()
 
     classifier.updateModel(patterns, weights)
     acc = classifier.classifyAll(allTesting)
-
+    stopwatch.stop()
+    stopwatch.print()
     fout = open(f"{host}.txt", "a")
     fout.write(f"{p},{w},{r},{round(acc,3)}\n")
     fout.close()
